3f564cc19b8bec34e5408a9bf4093fe4d1b694aa
Yi Heng <rainmrn@proton.me>
2024-08-04 07:36:34 +0800

    damage control

--- src/routes/(landing)/+page.svelte
+++ src/routes/(landing)/+page.svelte
@@ -14,54 +14,19 @@
 	aria-label="My Favorite Images"
 >
 	<SplideSlide>
-		<a href="" class="group">
 			<img src="/landing-carousell-1.jpg" />
-			<div
-				class="absolute bg-black bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 left-0 flex justify-center items-center duration-150 ease-in-out"
-			>
-				<p class="text-white font-bold text-xl opacity-0 group-hover:opacity-100">Something</p>
-			</div>
-		</a>
 	</SplideSlide>
 	<SplideSlide>
-		<a href="" class="group duration-150 ease-in-out">
 			<img src="/landing-carousell-2.jpg" />
-			<div
-				class="absolute bg-black bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 left-0 flex justify-center items-center duration-150 ease-in-out"
-			>
-				<p class="text-white font-bold text-xl opacity-0 group-hover:opacity-100">Something</p>
-			</div>
-		</a>
 	</SplideSlide>
 	<SplideSlide>
-		<a href="" class="group duration-150 ease-in-out">
 			<img src="/landing-carousell-3.jpg" />
-			<div
-				class="absolute bg-black bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 left-0 flex justify-center items-center duration-150 ease-in-out"
-			>
-				<p class="text-white font-bold text-xl opacity-0 group-hover:opacity-100">Something</p>
-			</div>
-		</a>
 	</SplideSlide>
 	<SplideSlide>
-		<a href="" class="group duration-150 ease-in-out">
 			<img src="/landing-carousell-4.jpg" />
-			<div
-				class="absolute bg-black bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 left-0 flex justify-center items-center duration-150 ease-in-out"
-			>
-				<p class="text-white font-bold text-xl opacity-0 group-hover:opacity-100">Something</p>
-			</div>
-		</a>
 	</SplideSlide>
 	<SplideSlide>
-		<a href="" class="group duration-150 ease-in-out">
 			<img src="/landing-carousell-5.jpg" />
-			<div
-				class="absolute bg-black bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 left-0 flex justify-center items-center duration-150 ease-in-out"
-			>
-				<p class="text-white font-bold text-xl opacity-0 group-hover:opacity-100">Something</p>
-			</div>
-		</a>
 	</SplideSlide>
 </Splide>
 
