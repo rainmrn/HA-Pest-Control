327d8e10e070deef6761b8523e3a49a225e91590
Yi Heng <rainmrn@proton.me>
2024-08-03 16:35:52 +0800

    chore: Update landing page design and styles

--- src/lib/Landing.svelte
+++ src/lib/Landing.svelte
@@ -3,16 +3,16 @@
 </script>
 
 <div id="bg" class="w-screen">
-		<Nav></Nav>
-		<div class="mx-auto w-3/4">
-            <div class="space-y-6 max-w-[50ch] pb-20">
-                <h1 class="text-white font-bold text-4xl uppercase">Explore your dream place with us.</h1>
-                <p class="text-white text-lg max-w-prose">
-                    Make the experience of traveling to your dream tourist destination come true with us. We
-                    will provide the best experience of your life.
-                </p>
-            </div>
-        </div>
+	<Nav></Nav>
+	<div class="mx-auto w-3/4">
+		<div class="space-y-6 max-w-[50ch] pb-20">
+			<h1 class="text-white font-bold text-4xl uppercase">Explore your dream place with us.</h1>
+			<p class="text-white text-lg max-w-prose">
+				Make the experience of traveling to your dream tourist destination come true with us. We
+				will provide the best experience of your life.
+			</p>
+		</div>
+	</div>
 </div>
 
 <style>
@@ -20,7 +20,7 @@
 		background-image: url('/landing.jpg');
 		background-size: cover;
 		background-position: center;
-		background-reapeat: no-repeat;
+		background-repeat: no-repeat;
 		background-color: rgba(0, 0, 0, 0.6);
 		background-blend-mode: darken;
 	}
--- src/routes/(landing)/+page.svelte
+++ src/routes/(landing)/+page.svelte
@@ -4,28 +4,66 @@
 </script>
 
 <Splide
-    options={{
-        type: 'loop',
-        focus: 'center',
-        perPage: 3,
-        heightRatio: 0.3,
-        gap: '1em'
-    }}
-    aria-label="My Favorite Images"
+	options={{
+		type: 'loop',
+		focus: 'center',
+		perPage: 3,
+		heightRatio: 0.4,
+		gap: '1em'
+	}}
+	aria-label="My Favorite Images"
 >
-    <SplideSlide>
-        <img src="/landing-carousell-1.jpg" />
-    </SplideSlide>
-    <SplideSlide>
-        <img src="/landing-carousell-2.jpg" />
-    </SplideSlide>
-    <SplideSlide>
-        <img src="/landing-carousell-3.jpg" />
-    </SplideSlide>
-    <SplideSlide>
-        <img src="/landing-carousell-4.jpg" />
-    </SplideSlide>
-    <SplideSlide>
-        <img src="/landing-carousell-5.jpg" />
-    </SplideSlide>
+	<SplideSlide>
+		<a href="" class="group">
+			<img src="/landing-carousell-1.jpg" />
+			<div
+				class="absolute bg-black bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 left-0 flex justify-center items-center duration-150 ease-in-out"
+			>
+				<p class="text-white font-bold text-xl opacity-0 group-hover:opacity-100">Something</p>
+			</div>
+		</a>
+	</SplideSlide>
+	<SplideSlide>
+		<a href="" class="group duration-150 ease-in-out">
+			<img src="/landing-carousell-2.jpg" />
+			<div
+				class="absolute bg-black bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 left-0 flex justify-center items-center duration-150 ease-in-out"
+			>
+				<p class="text-white font-bold text-xl opacity-0 group-hover:opacity-100">Something</p>
+			</div>
+		</a>
+	</SplideSlide>
+	<SplideSlide>
+		<a href="" class="group duration-150 ease-in-out">
+			<img src="/landing-carousell-3.jpg" />
+			<div
+				class="absolute bg-black bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 left-0 flex justify-center items-center duration-150 ease-in-out"
+			>
+				<p class="text-white font-bold text-xl opacity-0 group-hover:opacity-100">Something</p>
+			</div>
+		</a>
+	</SplideSlide>
+	<SplideSlide>
+		<a href="" class="group duration-150 ease-in-out">
+			<img src="/landing-carousell-4.jpg" />
+			<div
+				class="absolute bg-black bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 left-0 flex justify-center items-center duration-150 ease-in-out"
+			>
+				<p class="text-white font-bold text-xl opacity-0 group-hover:opacity-100">Something</p>
+			</div>
+		</a>
+	</SplideSlide>
+	<SplideSlide>
+		<a href="" class="group duration-150 ease-in-out">
+			<img src="/landing-carousell-5.jpg" />
+			<div
+				class="absolute bg-black bg-opacity-0 group-hover:bg-opacity-60 w-full h-full top-0 left-0 flex justify-center items-center duration-150 ease-in-out"
+			>
+				<p class="text-white font-bold text-xl opacity-0 group-hover:opacity-100">Something</p>
+			</div>
+		</a>
+	</SplideSlide>
 </Splide>
+
+<style>
+</style>
